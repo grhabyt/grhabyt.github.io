9c91a33207e4573cc7f20ed124c907df69dcfe77
grhabyt <rhabyt@gmail.com>
2021-10-14 18:16:44 -0700

    update

---
+++ project2/sketch.js
@@ -0,0 +1,25 @@
+//global
+var xloc1, yloc1;
+var xloc2, yloc2;
+var xloc3, yloc3;
+var firstLetter;
+var firstSwear;
+
+function setup() {
+  createCanvas(displayWidth, displayHeight);
+  var originalString = 'hello';
+  firstLetter = originalString.substring(0, 1);
+  firstSwear = originalString.substring(0, 4);
+  xloc1 = displayWidth/2;
+  xloc2 = displayWidth/2;
+  xloc3 = displayWidth/2;
+  yloc1 = displayHeight/2;
+  yloc2 = displayHeight/2;
+  yloc3 = displayHeight/2;
+}
+
+function draw() {
+  ellipse(50,50,xloc1, yloc1);
+  text(firstLetter,xloc2, yloc2);
+  text(firstSwear, xloc3, yloc3)
+}
