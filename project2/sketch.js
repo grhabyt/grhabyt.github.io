38aef77094acc58c9bb4621b5d4a3973275e7519
grhabyt <rhabyt@gmail.com>
2021-10-14 18:31:54 -0700

    Update sketch.js

--- project2/sketch.js
+++ project2/sketch.js
@@ -23,4 +23,10 @@ function draw() {
   ellipse(xloc1, yloc1,50,50);
   text(firstLetter,xloc2, yloc2);
   text(firstSwear, xloc3, yloc3);
+  xloc1 += random(-3,3);
+  xloc2 += random(-3,3);
+  xloc3 += random(-3,3);
+  yloc1 += random(-3,3);
+  yloc2 += random(-3,3);
+  yloc3 += random(-3,3);
 }
